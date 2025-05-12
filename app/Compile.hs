923ee092b894f65e5c9ed02e27e356f15ef92c22
uhkrg <uhkrg@student.kit.edu>
2025-05-12 23:35:28 +0200

    IR einführen

--- app/Compile.hs
+++ app/Compile.hs
@@ -4,14 +4,15 @@ module Compile
   )
 where
 
-import Compile.Asm (genAsm)
-import Compile.InstrSel (codeGen)
+--import Compile.Asm (genAsm)
+--import Compile.InstrSel (codeGen)
 import Compile.Parser (parseAST)
 import Compile.Semantic (semanticAnalysis)
-import Compile.VarAlloc (replaceVars)
+--import Compile.VarAlloc (replaceVars)
+import Compile.IR (translateAST)
 import Control.Monad.IO.Class
 import Error (L1ExceptT)
-import System.Process (readProcess)
+import Data.Array (elems)
 
 data Job = Job
   { src :: FilePath,
@@ -23,7 +24,8 @@ compile :: Job -> L1ExceptT ()
 compile job = do
   ast <- parseAST $ src job
   semanticAnalysis ast
-  let code = genAsm $ replaceVars $ codeGen ast
-  liftIO $ putStr $ unlines code
-  _ <- liftIO $ readProcess "gcc" ["-x", "assembler", "-o", out job, "-"] (unlines code)
+  liftIO $ putStrLn $ unlines $ map show $ elems $ translateAST ast
+  --let code = genAsm $ replaceVars $ codeGen ast
+  --liftIO $ putStr $ unlines code
+  --_ <- liftIO $ readProcess "gcc" ["-x", "assembler", "-o", out job, "-"] (unlines code)
   return ()
